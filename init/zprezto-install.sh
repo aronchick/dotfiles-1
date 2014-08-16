fbbe9464323b00350d532a741fb43e1fe218e5ee
Justin Garrison <justinleegarrison@gmail.com>
2014-08-16 16:48:23 -0700

    switching to zprezto for zshell config

---
+++ init/zprezto-install.sh
@@ -0,0 +1,8 @@
+export ZDOTDIR=$HOME/.dotfiles/src
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+mv "${ZDOTDIR:-$HOME}"/.zshenv ~/.zshenv
