6e0ee30d1bd75da6482de42197ea868b3314e922
Justin Garrison <justinleegarrison@gmail.com>
2014-10-22 13:56:21 -0700

    updated mouse map script :thumbup:

--- bin/evoluent-map.sh
+++ bin/evoluent-map.sh
@@ -1,1 +1,6 @@
-xinput set-button-map 11 1 2 3 4 5 6 7 8 9 10 11 12 13
+#!/bin/bash
+DEVICE="$(xinput | grep -i evoluent | sed -e 's/^.*id=\([0-9]*\).*$/\1/g')"
+#echo "Device found at input ${DEVICE}"
+if [ -n $DEVICE ]; then
+    xinput set-button-map ${DEVICE} 1 2 3 4 5 6 7 8 9 10 11 12 13
+fi
