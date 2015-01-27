4e56f3a3bf416f6e71000a8b312e7dd0afb0df69
Justin Garrison <justinleegarrison@gmail.com>
2015-01-27 09:57:10 -0800

    Updated evoluent script for proper var testing

--- bin/evoluent-map.sh
+++ bin/evoluent-map.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DEVICE="$(xinput | grep -i evoluent | sed -e 's/^.*id=\([0-9]*\).*$/\1/g')"
 #echo "Device found at input ${DEVICE}"
-if [ -n $DEVICE ]; then
+if [[ ! -z "$DEVICE" ]]; then
     xinput set-button-map ${DEVICE} 1 2 3 4 5 6 7 8 9 10 11 12 13
 fi
