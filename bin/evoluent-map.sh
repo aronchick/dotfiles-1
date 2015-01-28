d70a231153cb401ea2d91830409c69ffc1cecb26
Justin Garrison <justinleegarrison@gmail.com>
2015-01-28 11:16:34 -0800

    allow multiple mouse detection

--- bin/evoluent-map.sh
+++ bin/evoluent-map.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-DEVICE="$(xinput | grep -i evoluent | sed -e 's/^.*id=\([0-9]*\).*$/\1/g')"
+FILTER="Evoluent VerticalMouse 3"
+DEVICE="$(xinput | grep -i "${FILTER}" | sed -e 's/^.*id=\([0-9]*\).*$/\1/g')"
 #echo "Device found at input ${DEVICE}"
 if [[ ! -z "$DEVICE" ]]; then
-    xinput set-button-map ${DEVICE} 1 2 3 4 5 6 7 8 9 10 11 12 13
+    for i in $DEVICE; do
+      xinput set-button-map ${DEVICE} 1 2 3 4 5 6 7 8 9 10 11 12 13
+      echo "Set device $i"
+    done 
 fi
