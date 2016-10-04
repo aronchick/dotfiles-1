24895aac0543efb81b2f5cba96b662a6a7476c55
Justin Garrison <justinleegarrison@gmail.com>
2016-10-03 21:44:56 -0700

    fix kubectl completion ⎈

--- src/zsh.d/k8s.zsh
+++ src/zsh.d/k8s.zsh
@@ -1,7 +1,15 @@
-kc() {
-  if [ ! -z ${KUBE_NAMESPACE+x} ]; then
-    kubectl --namespace $KUBE_NAMESPACE "$@"
-  else
-    kubectl "$@"
-  fi
-}
+if command -v kubectl >/dev/null 2>&1; then
+  . <(kubectl completion zsh)
+
+  kc() {
+    if [ ! -z ${KUBE_NAMESPACE+x} ]; then
+      kubectl --namespace $KUBE_NAMESPACE "$@"
+    else
+      kubectl "$@"
+    fi
+  }
+  
+  alias kc='kubectl'
+  # get autocompletions from kubectl command
+  compdef kc=kubectl
+fi
