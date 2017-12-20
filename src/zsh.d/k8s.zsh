a4139059dcd7605fa96649b8c794b671308cb367
Justin Garrison <justinleegarrison@gmail.com>
2017-12-19 23:19:40 -0800

    Added kcns function

--- src/zsh.d/k8s.zsh
+++ src/zsh.d/k8s.zsh
@@ -1,15 +1,20 @@
 if [ $commands[kubectl] ]; then
   source <(kubectl completion zsh)
 
+  if [ $commands[fzf] ]; then
+    # helper for setting a namespace
+    # List namespaces, preview the pods within, and save as variable
+    function kcns() {
+        namespaces=$(kubectl get ns -o=custom-columns=:.metadata.name)
+        export KUBE_NAMESPACE=$(echo $namespaces | fzf --select-1 --preview "kubectl --namespace {} get pods")
+        echo "Set namespace to $KUBE_NAMESPACE"
+    }
+  fi
+
   kc() {
-    if [ ! -z ${KUBE_NAMESPACE+x} ]; then
-      kubectl --namespace $KUBE_NAMESPACE "$@"
-    else
-      kubectl "$@"
-    fi
+    kubectl --namespace ${KUBE_NAMESPACE:-default} "$@"
   }
   
-  alias kc='kubectl'
   # get autocompletions from kubectl command
   compdef kc=kubectl
   kcsh() { kc exec -i -t "$@" /bin/bash }
