da415dbf901e724da3183a07346a8338549061e2
Justin Garrison <justin.garrison@disneyanimation.com>
2018-03-19 15:46:40 -0700

    Update for mouse button mapping

--- bin/evoluent-map.sh
+++ bin/evoluent-map.sh
@@ -4,7 +4,7 @@ DEVICE="$(xinput | grep -i "${FILTER}" | sed -e 's/^.*id=\([0-9]*\).*$/\1/g')"
 #echo "Device found at input ${DEVICE}"
 if [[ ! -z "$DEVICE" ]]; then
     for i in $DEVICE; do
-      xinput set-button-map ${DEVICE} 1 2 3 4 5 6 7 8 9 10 11 12 13
+      xinput set-button-map ${DEVICE} 1 2 3 4 5 6 7 0 8 10 11 12 13
       echo "Set device $i"
     done 
 fi
